13904f6dd570d055c16d8a4110e04651d56f3a2a
Julio-Vasquez <jualvalitube@gmail.com>
2021-10-04 05:58:39 -0500

    feat(view): report

--- src/views/Report/Report.js
+++ src/views/Report/Report.js
@@ -1,5 +1,14 @@
 const Report = () => {
-  return <p>report</p>
+  return (
+    <iframe
+      width="100%"
+      height="760px"
+      title="Reporte Power BI"
+      src="https://app.powerbi.com/reportEmbed?reportId=641a581f-8e39-49d4-a563-1e0926150cfc&autoAuth=true&ctid=64ebc78e-0a5f-44e1-a504-5713eff64b33&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWNlbnRyYWwtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"
+      frameborder="0"
+      allowFullScreen="true"
+    ></iframe>
+  )
 }
 
 export default Report
